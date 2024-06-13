7fa21d5c59211c27bf3e65c48545fd40d99f7ca2
Ismailov Sarvarbek <72853160+ismailovsarvar@users.noreply.github.com>
2024-06-13 23:38:08 +0500

    Update form

--- app/admin.py
+++ app/admin.py
@@ -5,8 +5,19 @@ from app.models import Product, Image, Attribute, AttributeValue, ProductAttribu
 # Register your models here.
 
 
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Image)
 admin.site.register(Attribute)
 admin.site.register(AttributeValue)
 admin.site.register(ProductAttribute)
+
+
+# admin.site.unregister(Group)
+# admin.site.unregister(User)
+
+
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'discount', 'price')
+
+
+admin.site.register(Product, ProductModelAdmin)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from app.forms import ProductModelForm
 from app.models import Product
 
 
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-id')
     context = {
         'products': products
     }
@@ -21,3 +22,38 @@ def product_detail(request, product_id):
         'attributes': attributes
     }
     return render(request, 'app/product-details.html', context)
+
+
+# def add_product(request):
+#     form = ProductForm()
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)
+#         name = request.POST['name']
+#         description = request.POST['description']
+#         price = request.POST['price']
+#         rating = request.POST['rating']
+#         discount = request.POST['discount']
+#         quantity = request.POST['quantity']
+#         product = Product(name=name, description=description, price=price, rating=rating, discount=discount,
+#                           quantity=quantity)
+#         if form.is_valid():
+#             product.save()
+#             return redirect('index')
+#
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'app/add-product.html', context)
+
+def add_product(request):
+    form = ProductModelForm()
+    if request.method == 'POST':
+        form = ProductModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'app/add-product.html', context)
