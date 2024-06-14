5b54a352c98cd1a143acf11d64106f0c63b14c4a
Ismailov Sarvarbek <72853160+ismailovsarvar@users.noreply.github.com>
2024-06-15 00:20:09 +0500

    ADD, DELETE, UPDATE

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from app.views import customer_list, add_customer
 from config import settings
 
 urlpatterns = [
